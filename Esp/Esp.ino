5d858ce7be9b2f7cb80fdee0e9575628eed6fa8d
Isai Martinez <isai.martinez@yeico.com>
2023-03-09 14:49:16 -0600

    clean

--- Esp/Esp.ino
+++ Esp/Esp.ino
@@ -1,10 +1,9 @@
-
-
 int sensorPin = A0;    
 int ledPin = 11;      
 int sensorValue = 0;  
 float averageValue = 0;
 int count=0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);  
@@ -15,31 +14,19 @@ void loop() {
   // read the value from the sensor:
   sensorValue = analogRead(sensorPin);  
   
-  if(millis()<2000)
-  {
-      averageValue +=((float)sensorValue-averageValue)/10.f; 
-      
-      digitalWrite(ledPin,HIGH);
-    
- 
-      
-  }else 
-  {
-    
+  if(millis()<2000) {
+    averageValue +=((float)sensorValue-averageValue)/10.f; 
+    digitalWrite(ledPin,HIGH);
+  }else {
     if(sensorValue>averageValue +4){
-      count++;
+      count++; 
       Serial.println(count);
       averageValue =sensorValue+10;
-
       digitalWrite(ledPin,HIGH);
       delay(1000);
+    } else {
+      digitalWrite(ledPin,LOW);
+      averageValue +=((float)sensorValue-averageValue)/100.f; 
     }
-    else 
-    {
-    
-        digitalWrite(ledPin,LOW);
-        averageValue +=((float)sensorValue-averageValue)/100.f; 
-    }
-       
   }
 }
