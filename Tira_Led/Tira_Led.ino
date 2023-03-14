24349796da79dd3a3a045f5599a781339f0177ce
Isai Martinez <isai.martinez@yeico.com>
2023-03-14 12:44:54 -0600

    Color Code

--- Tira_Led/Tira_Led.ino
+++ Tira_Led/Tira_Led.ino
@@ -13,6 +13,7 @@ Adafruit_NeoPixel tira = Adafruit_NeoPixel(8, 12, NEO_GRB + NEO_KHZ800); // crea
 // con una cantidad de 8 pixeles, sobre pin 2 de Arduino y configurado para WS2812
 
 void setup(){
+  Serial.begin(9600);
   tira.begin();       // inicializacion de la tira
   tira.show();        // muestra datos en pixel
 }
@@ -20,7 +21,15 @@ void setup(){
 void loop(){
   tira.setBrightness(20);               // brillo global para toda la tira
   for(int i = 0; i < 8; i++){   // bucle para recorrer posiciones 0 a 7
-    tira.setPixelColor(i, 0, 0, 255);   // cada pixel en color azul (posicion,R,G,B)
+    Serial.println(i);
+    if(i <= 2) {
+      tira.setPixelColor(i, 0, 255, 0);   // cada pixel en color azul (posicion,R,G,B)
+    } else if(i <=5) {
+      tira.setPixelColor(i, 252, 232, 3);   // cada pixel en color azul (posicion,R,G,B)
+    } else {
+      tira.setPixelColor(i, 255, 0, 0);   // cada pixel en color azul (posicion,R,G,B)
+      
+    }
     tira.show();      // muestra datos en pixel 
     delay(500);       // breve demora de medio segundo
   }
