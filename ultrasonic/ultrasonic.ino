a5963ab03cc5fc71927921f3a11ee4a6c6eb053e
Isai Martinez <isai.martinez@yeico.com>
2023-03-16 15:53:01 -0600

    spaces

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -3,11 +3,13 @@ const int echoPin = 23;
 // defines variables
 long duration;
 int distance;
+
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600); // Starts the serial communication
 }
+
 void loop() {
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
